55beee358ec0b3e4a90a903662b5a7c7a17cfa81
johnfig14 <johnfig14@gmail.com>
2016-11-02 19:17:25 -0600

    Changes to the assignment of Thor

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,9 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-    'name' : 'Thor Anderson',
-    'gender' : 'Mail',
-    'age' : 49,
-    'interests' : ['Corvairs', 'JavaScript', 'Krispy Kreme'],
-    'bio' : 'Greatest professor ever. Always knows when to be more descriptive with his biography! Dang!  I should have known I did not have enough characters the first time around'
+    'name' : 'John Figueiredo',
+    'gender' : 'Male',
+    'age' : 25,
+    'interests' : ['Exercise', 'Reading', 'Computers', 'Music'],
+    'bio' : 'My name is John. I currently work as a Quality Assurance Software Specialist at Willis Towers Watson. I am currently taking a developer bootcamp through Neumont University; called Helio Training. I love it!'
 }
