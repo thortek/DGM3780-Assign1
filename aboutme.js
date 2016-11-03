efb4d428367cd1af11f8561390715841a3894547
Powrider <mattholmberg99@gmail.com>
2016-11-02 19:51:17 -0600

    added new object info and git ignore files.

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,16 @@
+exports.student = {
+    'Name ' : 'Matt Holmberg',
+    'Gender' : 'Male',
+    'Age' : 37,
+    'Interests' : ['Snowboarding', 'Road Trips', 'Movies'],
+    'Bio' : 'Hates being late to class.'
+}
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
-exports.student = {
-    'name' : 'Thor Anderson',
-    'gender' : 'Mail',
-    'age' : 49,
-    'interests' : ['Corvairs', 'JavaScript', 'Krispy Kreme'],
-    'bio' : 'Greatest professor ever. Always knows when to be more descriptive with his biography! Dang!  I should have known I did not have enough characters the first time around'
-}
+// exports.student = {
+//     'name' : 'Thor Anderson',
+//     'gender' : 'Mail',
+//     'age' : 49,
+//     'interests' : ['Corvairs', 'JavaScript', 'Krispy Kreme'],
+//     'bio' : 'Greatest professor ever. Always knows when to be more descriptive with his biography! Dang!  I should have known I did not have enough characters the first time around'
+// }
