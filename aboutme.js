a1bb0695cf656a893e32323bfd3c3b299c34a7b8
czdansie <czdansie@msn.com>
2016-11-02 19:53:00 -0600

    hid the idea files

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,10 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-    'name' : 'Thor Anderson',
-    'gender' : 'Mail',
-    'age' : 49,
-    'interests' : ['Corvairs', 'JavaScript', 'Krispy Kreme'],
-    'bio' : 'Greatest professor ever. Always knows when to be more descriptive with his biography! Dang!  I should have known I did not have enough characters the first time around'
+
+    'name' : "Cheralynn Dansie",
+    'gender' : 'female',
+    'age' : 33,
+    'interests' : ['Design', 'Events', 'Coding'],
+    'bio' : 'Mother of 2 girls, trying to learn how to code. I am not very interesting but I am trying to get to 140 characters.'
 }
