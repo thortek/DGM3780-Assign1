828409d4987ef7854238016242e6c14b72469b54
Scott Fayer <scott.fayer@gmail.com>
2016-11-02 19:45:32 -0600

    Scotts Assignment

--- aboutme.js
+++ aboutme.js
@@ -1,3 +1,10 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 
+var myObj = {
+    name: "Scott",
+    gender: "M",
+    age: 30,
+    interests: ["Snowboarding", "Hiking", "Life"],
+    bio: "This is my bio, it's not very interesting but at least it's kinda near the 140 Character requirement.  That's only 100 Characters so I have to add a little more."
+};
