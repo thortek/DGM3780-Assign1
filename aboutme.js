8db0cdac79250931b83129c02adb0032fdd2a1d9
jmkane <jkane76@outlook.com>
2016-11-02 19:05:20 -0600

    changed file to my information

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,8 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-    'name' : 'Thor Anderson',
-    'gender' : 'Mail',
-    'age' : 49,
-    'interests' : ['Corvairs', 'JavaScript', 'Krispy Kreme'],
-    'bio' : 'Greatest professor ever. Always knows when to be more descriptive with his biography! Dang!  I should have known I did not have enough characters the first time around'
-}
+    'name' : 'John Kane',
+    'gender' : 'Male',
+    'age' : 62,
+    'interests' : ['Running', 'JavaScript', 'Cooking'],
+    'bio' : 'Native Utahn, Learned Advanced Math in Montana, Switched to Computer Technolgy, worked in three Industry sectors, teaching mathematics as the last sector of life'
