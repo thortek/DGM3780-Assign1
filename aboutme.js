7e9080c5d6cf0b9e047b3f4e8576dca504c456a0
kdornseif <korvyn@gmail.com>
2016-11-02 19:16:54 -0600

    modified js object with my information

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,9 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-    'name' : 'Thor Anderson',
-    'gender' : 'Mail',
-    'age' : 49,
-    'interests' : ['Corvairs', 'JavaScript', 'Krispy Kreme'],
-    'bio' : 'Greatest professor ever. Always knows when to be more descriptive with his biography! Dang!  I should have known I did not have enough characters the first time around'
+    'name' : 'Korvyn Dornseif',
+    'gender' : 'Male',
+    'age' : 31,
+    'interests' : ['Disinterests', 'Starcraft2', 'Goute'],
+    'bio' : 'Frequently makes poor decisions.'
 }
