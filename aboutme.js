8e2baf99c3c4c17527b20b35d2a84f350b8a9a64
rderieux <ppldiecomplex@gmail.com>
2016-11-02 19:03:52 -0600

    Changed Thor's object information to my own info

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,9 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-    'name' : 'Thor Anderson',
-    'gender' : 'Mail',
-    'age' : 49,
-    'interests' : ['Corvairs', 'JavaScript', 'Krispy Kreme'],
-    'bio' : 'Greatest professor ever. Always knows when to be more descriptive with his biography! Dang!  I should have known I did not have enough characters the first time around'
-}
+    'name' : 'Robby DeRieux',
+    'gender' : 'Male',
+    'age' : 32,
+    'interests' : ['Video Games', 'JavaScript', 'Learning software developement'],
+    'bio' : 'Working really hard to wrap my mind around software developement.  It has been quite the journey but I feel that if I keep my head up and keep going I will eventually succeed!'
+};
