b8331b0d2532e5d1be1d8e6d2a286357d5e88702
millerman86 <amrenmiller@gmail.com>
2017-01-21 09:24:07 -0700

    Fixed spacing

--- aboutme.js
+++ aboutme.js
@@ -3,12 +3,12 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 var student = {
-    name: "Amren Miller",
-    gender: "Male",
-    age: 30,
-    interests: ["trumpet", "rap", "animation"],
+    name:"Amren Miller",
+    gender:"Male",
+    age:30,
+    interests:["trumpet", "rap", "animation"],
 
-    bio: "I'm just a regular guy who is trying to learn how to computer program. When I was\
+    bio:"I'm just a regular guy who is trying to learn how to computer program. When I was\
 approximately 20 years old, I attended the University of Utah to play the trumpet in their jazz band.\
 I haven't really completed any other higher or secondary education, but before I started this bootcamp, I worked at a\
 company called SmartSavers, where I was originally hired to develop an Android app."
