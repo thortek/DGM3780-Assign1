00bd8cb877c6fce837aeb562029aa962a89c5702
millerman86 <amrenmiller@gmail.com>
2017-01-21 08:37:03 -0700

    Add some declaration

--- aboutme.js
+++ aboutme.js
@@ -1,3 +1,8 @@
+var age;
+var gender;
+var age;
+var interests;
+var bio;
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 var javascriptobject = {
