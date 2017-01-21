253892a15a6cece2d2d39fa5a095a37de17ae0b3
millerman86 <amrenmiller@gmail.com>
2017-01-21 08:06:33 -0700

    Fix another declaration

--- aboutme.js
+++ aboutme.js
@@ -7,7 +7,7 @@ var age = 30;
 
 var myInterests = ["trumpet", "rap", "animation"];
 
-bio = "I'm just a regular guy who is trying to learn how to computer program. When I was\
+var bio = "I'm just a regular guy who is trying to learn how to computer program. When I was\
 approximately 20 years old, I attended the University of Utah to play the trumpet in their jazz band.\
 I haven't really completed any other higher or secondary education, but before I started this bootcamp, I worked at a\
 company called SmartSavers, where I was originally hired to develop an Android app."
