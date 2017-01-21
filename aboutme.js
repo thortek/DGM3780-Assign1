8b2fadd283800f2f9a0f7c10e3bea2ef9c4ad091
millerman86 <amrenmiller@gmail.com>
2017-01-21 08:05:26 -0700

    Fix declarations

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,9 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 
-name = "Amren Miller";
-gender = "Male";
-age = 30;
+var name = "Amren Miller";
+var gender = "Male";
+var age = 30;
 
 var myInterests = ["trumpet", "rap", "animation"];
 
