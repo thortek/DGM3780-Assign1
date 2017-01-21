cd47c913e70cf79740ca2f48b978b3fee722db98
millerman86 <amrenmiller@gmail.com>
2017-01-21 08:23:56 -0700

    Try this again

--- aboutme.js
+++ aboutme.js
@@ -1,7 +1,7 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 var javascriptobject = {
-    name = "Amren Miller";
+    name = "Amren Miller"
     gender = "Male";
     age = 30;
     myInterests = ["trumpet", "rap", "animation"];
